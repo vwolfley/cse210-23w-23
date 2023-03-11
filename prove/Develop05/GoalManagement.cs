b672b02273150d4dd5063bf3e5f1020f9f7eee57
vern wolfley <vern.wolfley@gmail.com>
2023-03-11 15:11:43 -0700

    working with new class of goalmanagement

--- prove/Develop05/GoalManagement.cs
+++ prove/Develop05/GoalManagement.cs
@@ -20,6 +20,77 @@ public class GoalManagement
 
 
     // Methods
-   
+    public void AddGoal(Goal goal)
+    {
+        _listGoals.Add(goal);
+    }
+
+    public void ListGoals()
+    {
+        Console.WriteLine("\nYour Goals are:");
+        // Loop though goals list
+        int index = 1;
+        foreach (Goal goal in _listGoals)
+        {
+            goal.ListGoal(index);
+            index = index + 1;
+        }
+    }
+
+    public void SaveGoals()
+    {
+        Console.Write("What is your name for this goal file?  ");
+        string userInput = Console.ReadLine();
+        string userFileName = userInput + ".txt";
+
+        using (StreamWriter outputFile = new StreamWriter(userFileName))
+        {
+            foreach (Goal g in _listGoals)
+            {
+                outputFile.WriteLine(g.SaveGoal());
+            }
+        }
+    }
+
+    public void LoadGoals()
+    {
+        Console.Write("What is the name of your goal file?  ");
+        string userInput = Console.ReadLine();
+        string userFileName = userInput + ".txt";
+
+        if (File.Exists(userFileName))
+        {
+            List<string> readText = File.ReadAllLines(userFileName).Where(arg => !string.IsNullOrWhiteSpace(arg)).ToList();
+            foreach (string line in readText)
+            {
+                string[] entries = line.Split("; ");
+
+                string type = entries[0];
+                string name = entries[1];
+                string description = entries[2];
+                int points = int.Parse(entries[3]);
+                bool status = Convert.ToBoolean(entries[4]);
+
+                if (entries[0] == "Simple Goal:")
+                {
+                    SimpleGoal sGoal = new SimpleGoal(type, name, description, points, status);
+                    AddGoal(sGoal);
+                }
+                if (entries[0] == "Eternal Goal:")
+                {
+                    EternalGoal eGoal = new EternalGoal(type, name, description, points, status);
+                    AddGoal(eGoal);
+                }
+                if (entries[0] == "Check List Goal:")
+                {
+                    int bonusNumber = int.Parse(entries[5]);
+                    int bonusPoints = int.Parse(entries[6]);
+                    ChecklistGoal clGoal = new ChecklistGoal(type, name, description, points, status, bonusNumber, bonusPoints);
+                    AddGoal(clGoal);
+                }
+            }
+        }
+    }
+
 
 }
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -8,12 +8,10 @@ class Program
     {
         // Use to convert text to title case
         TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
-        // List for all goals
-        List<Goal> goals = new List<Goal>();
 
+        GoalManagement goals = new GoalManagement();
 
-        // This will clear the console
-        Console.Clear();
+        Console.Clear();  // This will clear the console
         Console.Write("\n*** Welcome to the Eternal Quest Program ****\n");
 
         int totalPoints = 0;
@@ -34,8 +32,7 @@ class Program
             {
                 case 1:
                     // Create New Goal
-                    // This will clear the console
-                    Console.Clear();
+                    Console.Clear();  // This will clear the console
                     // Ask for user input (1-4)
                     int goalInput = 0;
                     while (goalInput != 4)
@@ -46,7 +43,6 @@ class Program
                         {
                             case 1:
                                 // Simple Goal
-                                // Console.WriteLine("\nSimple Goals\n");
                                 Console.WriteLine("What is the name of your goal?  ");
                                 string name = Console.ReadLine();
                                 name = textInfo.ToTitleCase(name);
@@ -56,12 +52,11 @@ class Program
                                 Console.WriteLine("What is the amount of points associated with this goal?  ");
                                 int points = int.Parse(Console.ReadLine());
                                 SimpleGoal sGoal = new SimpleGoal("Simple Goal:", name, description, points, false);
-                                goals.Add(sGoal);
+                                goals.AddGoal(sGoal);
                                 goalInput = 4;
                                 break;
                             case 2:
                                 // Eternal Goal
-                                // Console.WriteLine("Eternal Goals");
                                 Console.WriteLine("What is the name of your goal?  ");
                                 name = Console.ReadLine();
                                 name = textInfo.ToTitleCase(name);
@@ -71,12 +66,11 @@ class Program
                                 Console.WriteLine("What is the amount of points associated with this goal?  ");
                                 points = int.Parse(Console.ReadLine());
                                 EternalGoal eGoal = new EternalGoal("Eternal Goal:", name, description, points, false);
-                                goals.Add(eGoal);
+                                goals.AddGoal(eGoal);
                                 goalInput = 4;
                                 break;
                             case 3:
                                 // Checklist Goal
-                                // Console.WriteLine("Checklist Goals");
                                 Console.WriteLine("What is the name of your goal?  ");
                                 name = Console.ReadLine();
                                 name = textInfo.ToTitleCase(name);
@@ -90,7 +84,7 @@ class Program
                                 Console.WriteLine("What is the bonus for accomplishing it that many times?  ");
                                 int bonusPoints = int.Parse(Console.ReadLine());
                                 ChecklistGoal clGoal = new ChecklistGoal("Check List Goal:", name, description, points, false, bonusNumber, bonusPoints);
-                                goals.Add(clGoal);
+                                goals.AddGoal(clGoal);
                                 goalInput = 4;
                                 break;
                             case 4:
@@ -104,77 +98,20 @@ class Program
                     break;
                 case 2:
                     // List Goals
-                    // This will clear the console
-                    Console.Clear();
-
-                    Console.WriteLine("\nYour Goals are:");
-                    // Loop though goals list
-                    int index = 1;
-                    foreach (Goal goal in goals)
-                    {
-                        goal.ListGoal(index);
-                        index = index + 1;
-                    }
+                    Console.Clear();  // This will clear the console
+                    goals.ListGoals();
                     break;
                 case 3:
                     // Save Goals
-                    Console.Write("What is your name for this goal file?  ");
-                    string userInput = Console.ReadLine();
-                    string userFileName = userInput + ".txt";
-
-                    using (StreamWriter outputFile = new StreamWriter(userFileName))
-                    {
-                        foreach (Goal g in goals)
-                        {
-                            outputFile.WriteLine(g.SaveGoal());
-                        }
-                    }
-
+                    goals.SaveGoals();
                     break;
                 case 4:
                     // Load Goals
-                    Console.Write("What is the name of your goal file?  ");
-                    userInput = Console.ReadLine();
-                    userFileName = userInput + ".txt";
-
-                    if (File.Exists(userFileName))
-                    {
-                        List<string> readText = File.ReadAllLines(userFileName).Where(arg => !string.IsNullOrWhiteSpace(arg)).ToList();
-                        foreach (string line in readText)
-                        {
-                            string[] entries = line.Split("; ");
-
-                            string type = entries[0];
-                            string name = entries[1];
-                            string description = entries[2];
-                            int points = int.Parse(entries[3]);
-                            bool status = Convert.ToBoolean(entries[4]);
-
-                            if (entries[0] == "Simple Goal:")
-                            {
-                                SimpleGoal sGoal = new SimpleGoal(type, name, description, points, status);
-                                goals.Add(sGoal);
-                            }
-                            if (entries[0] == "Eternal Goal:")
-                            {
-                                EternalGoal eGoal = new EternalGoal(type, name, description, points, status);
-                                goals.Add(eGoal);
-                            }
-                            if (entries[0] == "Check List Goal:")
-                            {
-                                int bonusNumber = int.Parse(entries[5]);
-                                int bonusPoints = int.Parse(entries[6]);
-                                ChecklistGoal clGoal = new ChecklistGoal(type, name, description, points, status, bonusNumber, bonusPoints);
-                                goals.Add(clGoal);
-                            }
-                        }
-                    }
-
+                    goals.LoadGoals();
                     break;
                 case 5:
                     // Record Event
                     Console.WriteLine("record events");
-
                     break;
                 case 6:
                     // Quite
